99f49a19e04750b16f13ad7235524699c13d2abe
Ace Ladines <ajgladines2020@plm.edu.ph>
2023-04-17 22:30:13 +0800

    [ADD] Scholar Route

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const app = express()
-applicantsInfo = require('../models/application')
+const express = require("express");
+const app = express();
+applicantsInfo = require("../models/application");
 
 // Sub routes
-const pendingRouter = require('./admin-sub-routes/pending')
-app.use('/pending', pendingRouter)
+const pendingRouter = require("./admin-sub-routes/pending");
+app.use("/pending", pendingRouter);
 
-const completedReqRouter = require('./admin-sub-routes/completed-requirements')
-app.use('/completed-requirements', completedReqRouter)
+const completedReqRouter = require("./admin-sub-routes/completed-requirements");
+app.use("/completed-requirements", completedReqRouter);
 
-const candidateRouter = require('./admin-sub-routes/candidate-students')
-app.use('/candidate-students', candidateRouter)
+const candidateRouter = require("./admin-sub-routes/candidate-students");
+app.use("/candidate-students", candidateRouter);
 
-// const scholarRouter = require('./admin-sub-routes/scholar')
-// app.use('/scholar', scholarRouter)
+const scholarRouter = require("./admin-sub-routes/scholar");
+app.use("/scholar", scholarRouter);
 
-module.exports = app
+module.exports = app;
