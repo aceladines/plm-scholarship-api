4d68392caa681e2d46fc4a17f2780502d1f78385
aceladines <ajgladines2020@plm.edu.ph>
2023-04-21 22:24:04 +0800

    [FIX] Model paths

--- routes/opa.js
+++ routes/opa.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const applicantsInfo = require("../../models/application");
-const scholarships = require("../../models/scholarship");
+const applicantsInfo = require("../models/application");
+const scholarships = require("../models/scholarship");
 
 let options = {};
 
