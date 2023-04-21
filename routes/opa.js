4f311cd72a16f21389eff3cca074a848827e1894
aceladines <ajgladines2020@plm.edu.ph>
2023-04-21 15:53:55 +0800

    [ADD] Scholar tab with get functionality

---
+++ routes/opa.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const applicantsInfo = require("../../models/application");
+const scholarships = require("../../models/scholarship");
+
+let options = {};
+
+// * Default
+router.get("/*", async (req, res) => {
+  // * LIFO (Last In First Out)
+
+  const initialOption = await scholarships.findOne().sort({ _id: -1 }).exec();
+
+  if (initialOption && (req.query.provider === undefined || req.query.dateGiven === undefined)) {
+    options = {
+      approvalStatus: "SCHOLAR",
+      scholarshipProvider: initialOption.providerName,
+      dateOfBecomingScholar: initialOption.dateGiven[initialOption.dateGiven.length - 1].date,
+    };
+  } else {
+    options = {
+      approvalStatus: "SCHOLAR",
+      scholarshipProvider: req.query.provider,
+      dateOfBecomingScholar: req.query.dateGiven,
+    };
+  }
+
+  // * Get provider names and dateGiven
+  const providerNamesAndDateGiven = await scholarships.find().exec();
+
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 10;
+
+  try {
+    // * Create query object based on options
+    const query = {
+      $and: [
+        { approvalStatus: options.approvalStatus },
+        { scholarshipProvider: options.scholarshipProvider },
+        { dateOfBecomingScholar: options.dateOfBecomingScholar },
+      ],
+    };
+
+    // * execute query with page and limit values
+    const applicants = await applicantsInfo
+      .find(query)
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+
+    // * get total documents in the Posts collection
+    const count = await applicantsInfo.countDocuments({ $and: [options] });
+
+    // * Calculate total pages
+    const totalPages = Math.ceil(count / limit) || 1;
+
+    // * return response with posts, total pages, and current page
+    res.status(200).json({
+      applicants,
+      totalPages,
+      currentPage: page,
+      limit,
+      totalCount: count,
+      providerNamesAndDateGiven,
+    });
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+});
+
+module.exports = router;
