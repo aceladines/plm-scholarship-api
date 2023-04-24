527a6993e3c8cb7e19938e101549a038e9654f2c
aceladines <ajgladines2020@plm.edu.ph>
2023-04-25 07:32:39 +0800

    [REVERT] Removed await in line 106

--- routes/admin-sub-routes/pending.js
+++ routes/admin-sub-routes/pending.js
@@ -103,7 +103,7 @@ router.post("/approve", async (req, res) => {
     totalScore = gwaEquiv + incEquiv;
 
     try {
-      await applicantsInfo.findOneAndUpdate(
+      applicantsInfo.findOneAndUpdate(
         { email: user.email },
         {
           approvalStatus: "APPROVED",
