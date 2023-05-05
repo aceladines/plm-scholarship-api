ce806b7654fd0d7ab857963b10789769309d043e
aceladines <ajgladines2020@plm.edu.ph>
2023-05-06 02:23:00 +0800

    [ADD] Generate csv

--- routes/opa.js
+++ routes/opa.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const applicantsInfo = require("../models/application");
 const scholarships = require("../models/scholarship");
+const { AsyncParser } = require("@json2csv/node");
+const PDFDocument = require("pdfkit-table");
 
 let options = {};
-let dataToGenerate = [];
 
 router.get("/search", async (req, res) => {
   const searchParam = req.query.searchParam;
@@ -68,15 +69,126 @@ router.get("/search", async (req, res) => {
 
 // * Generate Report
 router.get("/generate-csv", async (req, res) => {
-  if (dataToGenerate.length === 0) {
-    res.status(500).json({ error: "No data to generate" });
-    return;
+  // let { startDate, endDate } = req.body;
+
+  // if (!startDate || !endDate)
+  //   return res.status(400).json({ error: "Please provide a start date and end date" });
+
+  let startDate = "2023-05-04";
+  let endDate = "2023-05-05";
+  let startDateCopy = startDate;
+  let endDateCopy = endDate;
+
+  startDate = new Date(startDate);
+  endDate = new Date(endDate);
+
+  const dataToGenerate = await applicantsInfo
+    .find({
+      $and: [{ approvalStatus: "SCHOLAR" }, { dateOfBecomingScholar: { $gte: startDate, $lte: endDate } }],
+    })
+    .sort({ dateOfBecomingScholar: 1 })
+    .select({
+      _id: 0,
+      scholarshipProvider: 1,
+      dateOfBecomingScholar: 1,
+      firstName: 1,
+      middleName: 1,
+      lastName: 1,
+      studentNum: 1,
+      course: 1,
+      college: 1,
+      dateApplied: 1,
+      currentGwa: 1,
+      year: 1,
+    })
+    .exec();
+
+  if (!dataToGenerate) return res.status(400).json({ error: "No data to generate" });
+
+  // * Make an array consisting unique dateOfBecomingScholar
+  let scholarships = [];
+
+  dataToGenerate.forEach((data) => {
+    if (
+      !scholarships.some(
+        (e) =>
+          e.program ===
+          `${data.scholarshipProvider} |  ${data.dateOfBecomingScholar
+            .toISOString()
+            .split("T")[0]
+            .trim()
+            .replace(/-/g, "/")}`
+      )
+    ) {
+      scholarships.push({
+        program: `${data.scholarshipProvider} |  ${data.dateOfBecomingScholar
+          .toISOString()
+          .split("T")[0]
+          .trim()
+          .replace(/-/g, "/")}`,
+        totalScholars: 0,
+        students: [],
+      });
+    }
+  });
+
+  // * Populate providersNameAndDateGiven with data
+
+  scholarships.forEach((data) => {
+    // * Get data from dataToGenerate
+    dataToGenerate.forEach((innerData) => {
+      if (
+        data.program.split("|")[0].trim() === innerData.scholarshipProvider &&
+        data.program.split("|")[1].trim() ===
+          innerData.dateOfBecomingScholar.toISOString().split("T")[0].trim().replace(/-/g, "/")
+      ) {
+        const dataToPush = {
+          "Student Name": `${innerData.firstName} ${innerData.middleName} ${innerData.lastName}`,
+          "Student No.": innerData.studentNum,
+          Course: `${innerData.course} `,
+          Year: `${innerData.year}`,
+          "Date Application": innerData.dateApplied.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }),
+          GWA: innerData.currentGwa,
+        };
+        data.students.push(dataToPush);
+        data.totalScholars++;
+      }
+    });
+  });
+
+  // * Sort the data in providersNameAndDateGiven by GWA
+  scholarships.forEach((data) => {
+    data.students.sort((a, b) => {
+      return a.currentGwa - b.currentGwa;
+    });
+  });
+
+  // * Get total scholars
+  let totalScholars = 0;
+  scholarships.forEach((data) => {
+    totalScholars += data.totalScholars;
+  });
+
+  let csvData = `START DATE - ${startDateCopy} | END DATE - ${endDateCopy} | TOTAL SCHOLARS: ${totalScholars}\n\n`;
+
+  for (let i = 0; i < scholarships.length; i++) {
+    const scholarship = scholarships[i];
+    csvData += `${scholarship.program.replace(/\|/g, "-")},,,,, Total ${scholarship.program
+      .split("|")[0]
+      .trim()} Scholars: ${scholarship.totalScholars}\n`;
+    const opts = {};
+    const transformOpts = {};
+    const parser = new AsyncParser(opts, transformOpts);
+    csvData += (await parser.parse(scholarship.students).promise()) + "\n\n";
   }
 
-  const { startDate, endDate } = req.body;
-
-  let data = [];
-  console.log("🚀 ~ file: opa.js:19 ~ router.get ~ data:", data);
+  res.setHeader("Content-Disposition", "attachment; filename=scholarships.csv");
+  res.set("Content-Type", "text/csv; charset=utf-8");
+  res.status(200).send(csvData);
 });
 
 // * Default
